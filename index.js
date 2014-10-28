6b14504e4cce7897c9a42b718d1dfad728a83d1b
Dylan Lingelbach <dylan.lingelbach@gmail.com>
2014-10-28 16:16:59 -0500

    Add console logging to script

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 var _ = require('underscore');
 var AWS = require('aws-sdk');
 var moment = require('moment');
-var path = require('path');
-var async = require('async');
-
-var Uploader = require('s3-streaming-upload').Uploader;
 
 var DynamoBackup = require('./lib/dynamo-backup');
 
 var runningAsScript = require.main === module;
 
 if (runningAsScript) {
-    var dynamoBackup = new DynamoBackup({ bucket: 'markitx-backups-test', stopOnFailure: true, readPercentage: .5, includedTables: ['development-asset-projects'] });
+    var runTimes = {};
+
+    var dynamoBackup = new DynamoBackup({ bucket: 'markitx-backups-test', stopOnFailure: true, readPercentage: .5 });
     dynamoBackup.on('error', function(data) {
         console.log('Error backing up ' + data.tableName);
         console.log(data.error);
         process.exit();
+    });
+    dynamoBackup.on('start-backup', function(tableName) {
+        runTimes[tableName] = moment();
+        console.log('Starting to copy table ' + tableName);
+    });
+    dynamoBackup.on('end-backup', function(tableName) {
+        var endTime = moment();
+        var startTime = runTimes[tableName];
+        console.log('Done copying table ' + tableName + '. Took ' + endTime.diff(startTime, 'minutes', true).toFixed(2) + ' minutes');
     });
 
     dynamoBackup.backupAllTables(function() {
