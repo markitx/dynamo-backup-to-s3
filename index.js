624313540b1035e573d74cf787b4e9e159f6d40e
Steven de Salas <steven@desalasworks.com>
2016-09-29 22:02:20 +1000

    Boilerplate for dynamo-restore-from-s3 process

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var DynamoBackup = require('./lib/dynamo-backup');
+var DynamoRestore = require('./lib/dynamo-restore');
+
+DynamoBackup.DynamoRestore = DynamoRestore;
 
 module.exports = DynamoBackup;
