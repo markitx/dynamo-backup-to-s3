92168e056495d52c0de822f0dda6dac45ba2866b
Steven de Salas <steven@desalasworks.com>
2016-10-05 22:19:15 +1100

    Use notation DynamoBackup.Restore.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var DynamoBackup = require('./lib/dynamo-backup');
 var DynamoRestore = require('./lib/dynamo-restore');
 
-DynamoBackup.DynamoRestore = DynamoRestore;
+DynamoBackup.Restore = DynamoRestore;
 
 module.exports = DynamoBackup;
